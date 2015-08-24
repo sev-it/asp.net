5e2883cb6c2c3427defd5075d0232b6bd9127235
Sayles <sayles.evp@gmail.com>
2015-08-24 18:46:06 +0300

    111

---
+++ App_Code/RegistrationInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для RegistrationInformation
+/// </summary>
+    public struct RegistrationInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string SelectedEvent { get; set; }
+    }
