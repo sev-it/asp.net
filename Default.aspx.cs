bdf0727dcaa93dc2b431f5e6f35ebd0524d31176
alekseev <alekseev@citypay.com.ua>
2015-08-24 18:24:36 +0300

    миша - всё по новой

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,8 +7,17 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page 
 {
-    protected void Page_Load(object sender, EventArgs e)
+    public RegistrationInformation RegistrationInformation
     {
-
+        get
+        {
+            return new RegistrationInformation()
+            {
+                FirstName = textFirstName.Text,
+                LastName = textLastName.Text,
+                Email = textLastName.Text,
+                SelectedEvent = dropDownListEvents.SelectedValue
+            };
+        }
     }
 }
