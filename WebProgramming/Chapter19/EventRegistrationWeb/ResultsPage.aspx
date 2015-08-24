65c05bb78851cdc98358b4a194c66dc3d35b4799
Sayles <sayles.evp@gmail.com>
2015-08-24 20:21:48 +0300

    Добавил текущий проект

---
+++ WebProgramming/Chapter19/EventRegistrationWeb/ResultsPage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ResultsPage.aspx.cs" CodeBehind="~/ResultsPage.aspx.cs" Inherits="_ResultsPage" %>
+<%@ PreviousPageType VirtualPath="~/Default.aspx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="labelResult" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebProgramming/Chapter19/EventRegistrationWeb/ResultsPage.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _ResultsPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            RegistrationInformation ri = PreviousPage.RegistrationInformation;
+            labelResult.Text = String.Format("{0} {1} selected the event {2}", 
+                               ri.FirstName, ri.LastName, ri.SelectedEvent);
+        }
+
+        catch
+        {
+            labelResult.Text = "The originating page must containt" +
+                               "textFirstName, textLastName, textEmail controls";
+        }
+
+    }
+}
