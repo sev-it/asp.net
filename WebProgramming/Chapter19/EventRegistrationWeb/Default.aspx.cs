04dbdf8a92c10e5359e37bc178cd6e78de7d95be
Sayles <sayles.evp@gmail.com>
2015-08-24 21:06:43 +0300

    456

---
+++ WebProgramming/Chapter19/EventRegistrationWeb/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    public RegistrationInformation RegistrationInformation
+    {
+        get
+        {
+            return new RegistrationInformation()
+            {
+                FirstName = textFirstName.Text,
+                LastName = textLastName.Text,
+                Email = textEmail.Text,
+                SelectedEvent = dropDownListEvents.SelectedValue,
+            };
+        }
+    }
+}
