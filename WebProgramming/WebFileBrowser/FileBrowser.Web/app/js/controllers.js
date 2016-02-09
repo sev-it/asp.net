7d712393be01fab08840254059541c3fd55493a8
Sayles <sayles.evp@gmail.com>
2016-02-09 11:57:11 +0300

    Добавлены комментарии по коду

--- WebProgramming/WebFileBrowser/FileBrowser.Web/app/js/controllers.js
+++ WebProgramming/WebFileBrowser/FileBrowser.Web/app/js/controllers.js
@@ -6,10 +6,12 @@ var WebControllers = angular.module('WebControllers', []);
 
 WebControllers.controller('DataController', ['$scope', 'Api',
 	function($scope, Api) {
+		// Задаём начальные значения для свойств
 		$scope.currentPath = "/";
 		$scope.drives = Api.Drive.get();
 		$scope.counters = {"result":{"less_ten":".","ten_to_fifty":".","over_hundred":"."}};
-			
+		
+		// Обработчик события перехода в каталог уровня n+1
 		$scope.goTo = function(path) {
 			if ($scope.currentPath.lastIndexOf("\\") == $scope.currentPath.indexOf("\\") && $scope.currentPath == "/")
 			{
@@ -23,11 +25,17 @@ WebControllers.controller('DataController', ['$scope', 'Api',
 			// Добавляем кнопку возврата на одну директорию назад:
 			$scope.btnBack = "..";
 			$scope.drives = "";
+			
+			// Отрисовываем дерево каталогов
 			$scope.subFolders = Api.Folder.get({path:$scope.currentPath});
+			
+			// Выводим информацию о количестве файлов
 			$scope.counters = Api.Counter.get({path:$scope.currentPath});
 		};
 		
+		// Обработчик события возврата в каталог уровня n-1
 		$scope.goBack = function() {
+			// Если находимся в корне диска, то запрашиваем список разделов
 			if($scope.currentPath.lastIndexOf("\\") == $scope.currentPath.indexOf("\\"))
 			{
 				$scope.currentPath = "/";
@@ -35,6 +43,8 @@ WebControllers.controller('DataController', ['$scope', 'Api',
 				$scope.drives = Api.Drive.get();
 				$scope.btnBack = "";
 			}
+			
+			// Если находимся не в корне диска, то переходим в родительский каталог
 			else
 			{
 				$scope.currentPath = $scope.currentPath.slice(0, $scope.currentPath.lastIndexOf("\\"));
--- WebProgramming/WebFileBrowser/FileBrowser.Web/app/js/services.js
+++ WebProgramming/WebFileBrowser/FileBrowser.Web/app/js/services.js
@@ -1,30 +1,15 @@
 'use strict';
 
 /* Services */
-
+// Сервис для отправки запросов к back-end
 var WebService = angular.module('WebService', ['ngResource']);
 
 WebService.factory('Api', ['$resource',
     function ($resource) {
-        //$http.defaults.useXDomain = true;
 		return {
-            /*getCounters: function (ext_path) {
-                return $resource('http://localhost:1393/api/counters/:path', {}, {
-                    GetFilesSize: {method: 'GET', params: {path:path}, isArray: true}
-                });
-            },
-            getData: function (ext_path) {
-                return get($resource('http://localhost:1393/api/directories/:path', {}, {
-                    GetData: {method: 'GET', params: {path:ext_path}}}));
-            },
-			getDrives: function () {
-                return $resource('http://localhost:1393/api/directories/', {}, {
-                    GetDrives: {method: 'GET'}
-                });
-            }*/
 			Drive: $resource('http://localhost:1393/api/directories/'),
-			Folder:  $resource('http://localhost:1393/api/directories/:path', {path: '1'}),
-			Counter:  $resource('http://localhost:1393/api/counters/:path', {path: '1'})
+			Folder:  $resource('http://localhost:1393/api/directories/:path', {path: '@path'}),
+			Counter:  $resource('http://localhost:1393/api/counters/:path', {path: '@path'})
         };
     }
 ]);
